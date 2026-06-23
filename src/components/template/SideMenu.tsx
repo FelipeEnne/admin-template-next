151b682166cb76fe8c334a59d5d0c7aac4715bc8
FelipeEnne <felipeenne@gmail.com>
2026-06-23 15:44:14 -0300

    logo component

--- src/components/template/SideMenu.tsx
+++ src/components/template/SideMenu.tsx
@@ -1,9 +1,15 @@
 import { IconAdjustments, IconBell, IconHome } from "../icons";
 import ItemMenu from "./ItemMenu";
+import Logo from "./Logo";
 
 export default function SideMenu() {
   return (
     <aside>
+      <div
+        className={`h-20 w-20 bg-linear-to-r from-indigo-500 to-purple-500 flex flex-col items-center justify-center`}
+      >
+        <Logo />
+      </div>
       <ul>
         <ItemMenu url="/" text="Home" icon={<IconHome />} />
         <ItemMenu
