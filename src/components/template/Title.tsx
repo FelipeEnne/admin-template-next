185382b84be1156ae812ee7fec002f98bc6d57d3
FelipeEnne <felipeenne@gmail.com>
2026-06-23 15:33:36 -0300

    title component

--- src/components/template/Title.tsx
+++ src/components/template/Title.tsx
@@ -8,8 +8,16 @@ interface TitleProps {
 export default function Title({ title, subtitle }: TitleProps) {
   return (
     <div className={`flex flex-col gap-2`}>
-      <h1 className={`flex flex-col gap-2`}>{title} </h1>
-      <h2 className={`flex flex-col gap-2`}>{subtitle}</h2>
+      <h1
+        className={`flex flex-col gap-2 font-black text-3xl text-gray-900 dark:text-gray-100`}
+      >
+        {title}{" "}
+      </h1>
+      <h2
+        className={`flex flex-col gap-2 font-light text-sm text-gray-600 dark:text-gray-400`}
+      >
+        {subtitle}
+      </h2>
     </div>
   );
 }
