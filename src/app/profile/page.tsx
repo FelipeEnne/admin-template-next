b1debcce3cffd07fbdcfada2bf578139dc91b699
FelipeEnne <felipeenne@gmail.com>
2026-07-21 18:53:09 -0300

    fix: image

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import Layout from "@/components/template/Layout";
-import useAppData from "@/data/hook/useAppData";
 
 export default function Profile() {
-  const ctx = useAppData();
-
   return (
     <Layout title="Profile" subtitle="Here you can manage your profile">
       <h1>Profile</h1>
