4bfb1930679007ba9f33c648e21702406a5d8c98
FelipeEnne <felipeenne@gmail.com>
2026-07-23 15:11:29 -0300

    genereate session with cookie

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -2,13 +2,15 @@
 
 import { useRouter } from "next/navigation";
 import { auth } from "@/firebase/config";
+import Cookies from "js-cookie";
 import User from "@/model/User";
 import {
   GoogleAuthProvider,
   signInWithPopup,
   type User as FirebaseUser,
+  onIdTokenChanged,
 } from "firebase/auth";
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 interface AuthContextProps {
   user: User | null;
@@ -32,22 +34,45 @@ async function normalizeUser(userFirebase: FirebaseUser): Promise<User> {
   };
 }
 
+function cookieManager(logged: boolean) {
+  if (logged) {
+    Cookies.set("admin-template-auth", String(logged), { expires: 7 });
+  } else {
+    Cookies.remove("admin-template-auth");
+  }
+}
+
 export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [loading, setLoading] = useState(true);
   const [user, setUser] = useState<User | null>(null);
   const router = useRouter();
+
+  async function configSession(userFirebase: FirebaseUser | null) {
+    if (userFirebase?.emailVerified) {
+      const user = await normalizeUser(userFirebase);
+      setUser(user);
+      cookieManager(true);
+      setLoading(false);
+      return user.email;
+    } else {
+      setUser(null);
+      cookieManager(false);
+      setLoading(false);
+      return false;
+    }
+  }
 
   async function loginGoogle() {
     const provider = new GoogleAuthProvider();
     const resp = await signInWithPopup(auth, provider);
-    if (resp.user?.emailVerified) {
-      const user = await normalizeUser(resp.user);
-      setUser(user);
-      await auth.currentUser?.getIdToken(true);
-      router.push("/");
-    } else {
-      throw new Error("Google login failed");
-    }
+    await configSession(resp.user);
+    router.push("/");
   }
+
+  useEffect(() => {
+    const cancel = onIdTokenChanged(auth, configSession);
+    return () => cancel();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user, loginGoogle }}>
