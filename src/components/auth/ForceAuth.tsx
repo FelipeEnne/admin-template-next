c46cb57c4b922032fbce480ae5700c77486bcc6f
FelipeEnne <felipeenne@gmail.com>
2026-08-04 19:25:54 -0300

    feat: protect roots

--- src/components/auth/ForceAuth.tsx
+++ src/components/auth/ForceAuth.tsx
@@ -1,11 +1,27 @@
 import useAuth from "@/data/hook/useAuth";
+import Head from "next/head";
 import { useRouter } from "next/navigation";
 
 export default function ForceAuth({ children }: { children: React.ReactNode }) {
   const { user, loading } = useAuth();
   const router = useRouter();
   function renderContent() {
-    return <>{children}</>;
+    return (
+      <>
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+      if (!document.cookie.includes("admin-template-auth")) {
+        window.location.href = "/authentication";
+      }
+    `,
+            }}
+          ></script>
+        </Head>
+        {children}
+      </>
+    );
   }
 
   function renderLoading() {
